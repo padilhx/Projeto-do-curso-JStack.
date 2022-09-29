7f42409c0b53bed81b9a7fb9fbffa82580803886
guilherme padilha jobins <guilhermepjobins@hotmail.com>
2022-09-29 14:45:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useMemo, useEffect, useRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+
+import themes from './styles/themes';
+
+function App() {
+
+  const [theme, setTheme] = useState('dark');
+
+  const firstRender = useRef(true);
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+
+    console.debug({ theme });
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyle />
+      <Layout
+        onToggleTheme={handleToggleTheme}
+        selectedTheme={theme}
+      />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Footer({ onToggleTheme, selectedTheme, }) {
+  return (
+    <Container>
+      <span>JStack's Blog. Todos os direitos reservados.</span>
+      <button type="button" onClick={onToggleTheme}
+      >
+        {selectedTheme === 'dark' ? '🌞' : '🌚'}
+      </button>
+    </Container>
+  );
+}
